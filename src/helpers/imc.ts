1990846136daa1a6a145c2fcca93a1f84779e7d3
micas-tsx <micaelabud21@gmail.com>
2025-07-24 07:43:32 -0300

    feat: start to make the project functional

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -37,10 +37,9 @@ export const calculateImc = (height: number, weight: number) => {
   const imc = weight / (height * height);
 
   for(let i in levels) {
-    if(imc >= levels[i].imc[0] && 
-       imc <= levels[i].imc[1]) {
-        levels[i].yourImc = imc;
-        return levels[1]
+    if(imc >= levels[i].imc[0] && imc <= levels[i].imc[1]) {
+        levels[i].yourImc = parseFloat(imc.toFixed(2));
+        return levels[i]
     }
   }
 
